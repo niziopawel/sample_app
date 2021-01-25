8aa67ab2519cda928b44884a3db85cb4efba6825
niziopawel <niziopawel2@gmail.com>
2021-01-25 18:09:56 +0100

    Add bootstrap, navigation between static pages, basic tests

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   # for root address call hello method from application controller
   root 'static_pages#home' 
 
-  get 'static_pages/home'
-  get 'static_pages/help'
-  get 'static_pages/contact'
-  get 'static_pages/about'
+  get '/help', to: 'static_pages#help'
+  get '/contact', to: 'static_pages#contact'
+  get '/about', to: 'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
